dcb810a66540fb004cbd7e5f2fc5bcd26fa50b19
SneakySnekk <rami.vaisanen@gmail.com>
2023-04-05 15:26:42 +0300

    Added scripts

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -17,11 +17,17 @@ public class PlayerControl : MonoBehaviour
     {
         //if(Input.GetKey(KeyCode.Space))
         if (Input.GetButton("Jump"))
-        rb.AddForce(new Vector2(0, 30));
-    float xInput = Input.GetAxisRaw("Horizontal");
-    float yInput = Input.GetAxisRaw("Vertical");
+        
+        rb.AddForce (transform.up*5);
 
-    gameObject.transform.Translate(new Vector3(xInput * Time.deltaTime, yInput * Time.deltaTime));
+        //rb.AddForce (New.vector2(0,30));
+
+
+    //float xInput = Input.GetAxisRaw("Horizontal");
+    gameObject.transform.Rotate(0, 0, -Input.GetAxis("Horizontal"));
+    //float yInput = Input.GetAxisRaw("Vertical");
+
+   // gameObject.transform.Translate(new Vector3(xInput * Time.deltaTime, yInput * Time.deltaTime));
 
         //if (Input.GetAxis(""));
     
